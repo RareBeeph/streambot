64ac22efd6ee66c2400a85527122182a7107cead
Keiran Jensen <keiranjensen@gmail.com>
2024-04-08 19:34:43 -0400

    bugfix

--- bot/bot.go
+++ bot/bot.go
@@ -72,19 +72,30 @@ func (b *bot) registerCommands() error {
 		if err != nil {
 			return err
 		}
-		query.RegisteredCommand.Create(&models.RegisteredCommand{ID: reg.ID, GuildID: reg.GuildID})
+
+		err = query.RegisteredCommand.Create(&models.RegisteredCommand{ID: reg.ID, GuildID: reg.GuildID})
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
 func (b *bot) unregisterCommands() error {
-	allcommands, err := query.RegisteredCommand.Find()
+	rc := query.RegisteredCommand
+
+	allcommands, err := rc.Find()
 	if err != nil {
 		return err
 	}
 
 	for _, c := range allcommands {
 		err = b.session.ApplicationCommandDelete(b.session.State.User.ID, c.GuildID, c.ID)
+		if err != nil {
+			return err
+		}
+
+		_, err = rc.Where(rc.ID.Eq(c.ID)).Delete()
 		if err != nil {
 			return err
 		}
