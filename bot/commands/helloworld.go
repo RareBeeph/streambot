7dc92979f1b132530903f697f4fc6ac84749624e
Keiran Jensen <keiranjensen@gmail.com>
2024-04-11 19:07:42 -0400

    ari's patch

--- bot/commands/helloworld.go
+++ bot/commands/helloworld.go
@@ -2,19 +2,21 @@ package commands
 
 import "github.com/bwmarrin/discordgo"
 
+var helloWorldCmd = &Definition{
+	Name: "helloworld",
+	Base: &discordgo.ApplicationCommand{
+		Description: "hello world",
+	},
+	Handler: func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "hello world",
+			},
+		})
+	},
+}
+
 func init() {
-	Register(&Definition{
-		Name: "helloworld",
-		Base: &discordgo.ApplicationCommand{
-			Description: "hello world",
-		},
-		Handler: func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionResponseData{
-					Content: "hello world",
-				},
-			})
-		},
-	})
+	Register(helloWorldCmd)
 }
