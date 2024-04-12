c2d02a46d394b3b984b5f59561a7b9dd223a9214
Keiran Jensen <keiranjensen@gmail.com>
2024-04-12 19:42:56 -0400

    prototype subscribe command

--- bot/commands/subscribe.go
+++ bot/commands/subscribe.go
@@ -1,6 +1,14 @@
 package commands
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"log"
+	"streambot/bot/twitch"
+	"streambot/models"
+	"streambot/query"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/nicklaw5/helix/v2"
+)
 
 var subscribeCmd = &Definition{
 	Name: "subscribe",
@@ -24,15 +32,42 @@ var subscribeCmd = &Definition{
 	Handler: func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		options := i.ApplicationCommandData().Options
 
-		optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
+		optionValues := make(map[string]string, len(options))
 		for _, opt := range options {
-			optionMap[opt.Name] = opt
+			optionValues[opt.Name] = opt.StringValue()
 		}
 
+		gamesResponse, err := twitch.Client.GetGames(&helix.GamesParams{
+			Names: []string{optionValues["game_name"]},
+		})
+		if err != nil {
+			log.Println("I have no idea what to do here") // Temp
+		} else if len(gamesResponse.Data.Games) == 0 {
+			log.Println("No games found") // Temp
+		} else if len(gamesResponse.Data.Games) > 1 {
+			log.Println("More than one game with the same title???") // Temp
+		} else {
+			qs := query.Subscription
+
+			game := gamesResponse.Data.Games[0]
+
+			sub := &models.Subscription{
+				GameName:  game.Name,
+				GameID:    game.ID,
+				Filter:    optionValues["optional_filter"],
+				GuildID:   i.GuildID,
+				ChannelID: i.ChannelID,
+			}
+
+			qs.Create(sub) // TODO: handle potential error
+			qs.Delete(sub) // Temp because I don't have an unsubscribe command yet
+		}
+
+		// Temp; echo
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
-				Content: optionMap["game_name"].StringValue() + " " + optionMap["optional_filter"].StringValue(),
+				Content: optionValues["game_name"] + " " + optionValues["optional_filter"],
 			},
 		})
 	},
