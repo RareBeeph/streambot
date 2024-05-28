aa4430bf6e9af9135eb0c71c2061adfcef73de07
Aria Taylor <ari@aricodes.net>
2024-05-28 16:01:14 -0700

    Assign and acquire a mutex for each task

--- bot/tasks/tasks.go
+++ bot/tasks/tasks.go
@@ -1,6 +1,8 @@
 package tasks
 
 import (
+	"sync"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/rs/zerolog/log"
 )
@@ -11,6 +13,7 @@ type Task struct {
 	handler      func(*discordgo.Session)
 	runOnStartup bool
 	dg           *discordgo.Session
+	mu           sync.Mutex
 }
 
 func (t *Task) BindSession(s *discordgo.Session) {
@@ -20,7 +23,15 @@ func (t *Task) BindSession(s *discordgo.Session) {
 func (t *Task) Run() {
 	if t.dg == nil {
 		log.Error().Msg("Cannot run tasks before binding a session to them.")
+		return
 	}
+
+	if locked := t.mu.TryLock(); !locked {
+		// Maybe we should add a Name field so that we can improve this output?
+		log.Error().Msg("Skipping task execution, prior run still going")
+		return
+	}
+	defer t.mu.Unlock()
 
 	t.handler(t.dg)
 }
